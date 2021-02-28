d89d9e9fd9b5a8e311c957159b9fbdcdd45630f0
Simon Hammes <simonhammes.27@gmail.com>
2021-02-28 11:44:18 +0100

    Update FooTest.php

--- tests/Integration/FooTest.php
+++ tests/Integration/FooTest.php
@@ -44,6 +44,25 @@ class FooTest extends WP_UnitTestCase {
 		
 		$plugin->run();
 		
-		static::assertNotFalse( has_action( 'plugins_loaded', [ $plugin, 'load_textdomain' ] ), 'Loading textdomain is not hooked in correctly.' );
+		static::assertIsInt( has_action( 'plugins_loaded', [ $plugin, 'load_textdomain' ] ), 'Loading textdomain is not hooked in correctly.' );
+	}
+	
+	public function test_custom_action_to_plugin_textdomain_is_added(): void {
+		$mock_config = [
+			'Settings' => [],
+			'Plugin'   => [
+				'textdomain'    => 'apple',
+				'languages_dir' => 'banana',
+			],
+		];
+
+		$mock_config = ConfigFactory::createFromArray( $mock_config );
+		
+		// Replace this with some actual integration testing code.
+		$plugin = new \Gamajo\PluginSlug\Plugin($mock_config);
+		
+		$plugin->run();
+		
+		static::assertIsInt( has_action( 'init', [ $plugin, 'action1' ] ) );
 	}
 }
